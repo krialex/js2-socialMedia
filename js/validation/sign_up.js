47a8c03b1a6bff0883c4bae894e8e71138b0756a
krialex <56829037+krialex@users.noreply.github.com>
2024-06-26 11:14:03 +0200

    added files that were missing, login and signin

---
+++ js/validation/sign_up.js
@@ -0,0 +1,78 @@
+import { GET_BASE_URL, REGISTRER } from "../variabler.js";
+
+
+const registreringForm = document.querySelector(".signUpForm");
+const userName = document.getElementById("loginName");
+const userEmail = document.getElementById("loginEmail");
+const userPassword = document.getElementById("loginPassword");
+const errorName = document.getElementById("errorName");
+const errorEmail = document.getElementById("errorEmail");
+const errorPassword = document.getElementById("errorPassword");
+
+
+// Get api to sign up:
+export async function registrerAccount(name, email, password) {
+    try {
+        const response = await fetch(GET_BASE_URL + REGISTRER, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log(result); 
+        } else {
+            throw new Error("Could not registrer account");
+        }}
+    catch(error) {
+        console.log(error);
+    }
+};
+
+
+export function validateEmail(email) {
+    const pattern = /@stud\.noroff\.no$/;
+    return pattern.test(email);
+}
+
+registreringForm.addEventListener("submit", async (event) => {
+    event.preventDefault(); 
+
+    let isValid = true;
+
+    if (userName.value.trim() === "") {
+        errorName.style.display = "block";
+        isValid = false;
+    } else {
+        errorName.style.display = "none";
+    }
+    if (userEmail.value.trim() === "" || !validateEmail(userEmail.value)) {
+        errorEmail.style.display = "block";
+        isValid = false;
+    } else {
+        errorEmail.style.display = "none";
+    }
+    if (userPassword.value.trim().length < 8) {
+        errorPassword.style.display = "block";
+        isValid = false;
+    } else {
+        errorPassword.style.display = "none";
+    }
+
+    if (isValid) {
+        const name = userName.value;
+        const email = userEmail.value;
+        const password = userPassword.value;
+        console.log(name, email, password);
+
+        try {
+            await registrerAccount(name, email, password); 
+            setTimeout(() => window.location.replace("index.html"), 1000);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
